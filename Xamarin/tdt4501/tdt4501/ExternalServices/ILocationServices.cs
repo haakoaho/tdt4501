23809732acaa3837c57ed4d6806cd6a2cb22d465
haakoaho <haakoaho@stud.ntnu.no>
2019-11-06 11:56:55 +0100

    Create ILocationServices.cs

---
+++ Xamarin/tdt4501/tdt4501/ExternalServices/ILocationServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tdt4501.ExternalServices
+{
+    interface ILocationServices
+    {
+        Task<Tuple<int,int>> GetGeoCoordinatesAsync();
+    }
+}
