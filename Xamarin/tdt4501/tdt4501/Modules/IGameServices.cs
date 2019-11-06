d2fbdcb583d1ad028fd42eb9e182eecc8987c3dc
haakoaho <haakoaho@stud.ntnu.no>
2019-11-06 22:27:04 +0100

    Testing stuff

--- Xamarin/tdt4501/tdt4501/Modules/IGameServices.cs
+++ Xamarin/tdt4501/tdt4501/Modules/IGameServices.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-
+using tdt4501.Models;
 
 namespace tdt4501.ExternalServices
 {
     //for injecting
     //ILocationServices locationServices = DependencyService.Get<ILocationServices>();
     //this is a tutorial for using Native service can be deleted
-    public interface ILocationServices
+    public interface IGameServices
     {
-        Tuple<int,int> GetGeoCoordinates();
+        void EnterMultiPlayerLobby();
+        void UnlockAchievement(String Achievement);
+        void SendData(Byte data);
+        Byte ReceiveData();
     }
 }
